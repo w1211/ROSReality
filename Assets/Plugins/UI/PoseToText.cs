491191cdc6807ae849164f7d0d8736eb5e110478
Declan Scott <declanjscott@gmail.com>
2018-10-26 19:14:37 +1100

    Add files via upload

---
+++ Assets/Plugins/UI/PoseToText.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PoseToText : MonoBehaviour {
+
+    public Text m_TextObject;
+
+    public GameObject TargetObject;
+
+    public float multiplier = 10;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_TextObject.text = "Position: " +  ((TargetObject.transform.position)  * multiplier).ToString();
+    }
+}
---
+++ Assets/Plugins/UI/RotationToText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RotationToText : MonoBehaviour {
+
+    public Text m_TextObject;
+
+    public GameObject TargetObject;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        m_TextObject.text = "Rotation: " + (TargetObject.transform.rotation).ToString();
+    }
+}
